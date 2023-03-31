d99a069f1a1fc13662b7b3230a275314bedc1d4c
monninen <onni.hassila@gmail.com>
2023-03-31 16:35:28 +0300

    Hyvin rough tappelusysteemin alku :3

---
+++ Model/Fighter.java
@@ -0,0 +1,7 @@
+package Model;
+
+public interface Fighter {
+    public int proceed();
+    public int performAction();
+    public void die();
+}                                                                                                         
