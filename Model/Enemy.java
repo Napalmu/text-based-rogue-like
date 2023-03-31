2ac3eadd21182c4ac6102269ae13d388acd1365a
Joonatan <joonatan.merenluoto@gmail.com>
2023-03-31 15:29:20 +0300

    Päivitä Entity -luokka ja luo Enemy -luokka, joka perii Entityn.

---
+++ Model/Enemy.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Enemy extends Entity{
+    public Enemy(int hp, String name){
+        super(hp, name);
+    }
+}
--- Model/Entity.java
+++ Model/Entity.java
@@ -1,5 +1,25 @@
 package Model;
 
 public class Entity {
-    
+    private int hp;
+    private String name;
+
+    public Entity(int hp, String name){
+        this.hp = hp;
+        this.name = name;
+    }
+    public int getHp(){
+      return hp;  
+    }
+    public String getName(){
+        return name;
+    }
+    public void setHp(int hp){
+        this.hp = hp;
+    }
+    public void setName(String name){
+        this.name = name;
+    }    
+
 }
+
