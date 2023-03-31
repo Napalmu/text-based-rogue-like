751fac6020e777805010dd4ff94615689a4a0173
Joonatan <joonatan.merenluoto@gmail.com>
2023-03-31 15:09:10 +0300

    Add Player.java

---
+++ Model/Player.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Player {
+    private int hp;
+
+    public Player(int hp){
+        this.hp = hp;
+    }
+    public void setHp(int hp){
+        this.hp = hp;
+    }
+    public int getHp(){
+        return hp;
+    }
+
+}
+
