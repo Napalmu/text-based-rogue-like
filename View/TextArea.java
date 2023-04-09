904d29b8a35ce6ab239745ca7ae4b0f6f8720fc5
Erno Lokkila <eolokk@utu.fi>
2023-04-09 13:41:49 +0300

    Text area kuuntelee mallin lähettämiä eventtejä

--- View/TextArea.java
+++ View/TextArea.java
@@ -1,9 +1,17 @@
 package View;
 
+import Model.GameEventManager;
+import Model.Item;
+
 public class TextArea extends DrawArea{
 
     public TextArea(int x, int y, int width, int height) {
         super(x, y, width, height);
+        GameEventManager.registerListener(this::itemReceived);
+    }
+
+    public void itemReceived(Item i){
+        System.out.println("Saatiin tavara "+i.getName());
     }
     
 }
