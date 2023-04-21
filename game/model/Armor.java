1a37ce8e26e4fa2eac985bd88101b0de82c53727
Joonatan <joonatan.merenluoto@gmail.com>
2023-04-21 14:32:02 +0300

    Add Item children (Consumable, Equipment[Armor, Weapon])

---
+++ game/model/Armor.java
@@ -0,0 +1,12 @@
+package game.model;
+
+import game.controller.ItemType;
+
+public class Armor extends Equipment{
+
+    public Armor(ItemType type) {
+        super(type);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ game/model/Consumable.java
@@ -0,0 +1,12 @@
+package game.model;
+
+import game.controller.ItemType;
+
+public class Consumable extends Item {
+
+    public Consumable(ItemType type) {
+        super(type);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
