f6c5425e4fe52797f15636c804b9d7fcde13e76e
Erno Lokkila <eolokk@utu.fi>
2023-04-23 17:27:06 +0300

    Yksinkertaistin DrawCommand perintähierarkiaa

--- game/view/TextArea.java
+++ game/view/TextArea.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 
- class TextArea extends DrawArea{
+ class TextArea extends ParentDrawCommand{
 
     TextArea(){this(59,4);}
      TextArea(int x, int y) {
