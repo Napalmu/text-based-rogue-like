070cba1b9c2dae897dc35a5c4b54378144e5a74a
Embalistico <eemeliheikkanen@gmail.com>
2023-05-12 14:56:41 +0300

    EnemyType enumit olemassa, ei toimintaa

--- game/controller/EnemyType.java
+++ game/controller/EnemyType.java
@@ -1,5 +1,43 @@
 package game.controller;
 
-public enum EnemyType {
-    GOBLIN, SKELETON, GHOST
+public enum EnemyType implements GameConcept{
+    GOBLIN {
+
+        @Override
+        public String getName() {
+            return "Hiisi";
+        }
+
+        @Override
+        public int GetHp() {
+            return 30;
+        }
+
+        }, SKELETON{
+            
+        @Override
+        public String getName() {
+            return "Luuranko";
+        }
+
+        @Override
+        public int GetHp() {
+            return 40;
+        }
+        }, GHOST{
+        
+        @Override
+        public String getName() {
+            return "Aave";
+        }
+
+        @Override
+        public int GetHp() {
+            return 60;
+        }
+        },;
+
+    public abstract String getName();
+    public abstract int GetHp();
+    /*public abstract int getSpeed();*/
 }
